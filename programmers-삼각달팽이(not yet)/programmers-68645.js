dec37ea3d2bf37df9078ab1aaebd45c8e3ff77f5
Inseob Jeon <sonofhuman20@gmail.com>
2021-01-16 08:08:15 +0900

    read the description of H-index in a wrong way...

--- programmers-삼각달팽이(not yet)/programmers-68645.js
+++ programmers-삼각달팽이(not yet)/programmers-68645.js
@@ -1,7 +1,5 @@
 const solution = (number, k) => {
-    const sortedNumberArr = [...number].map(arg => Number(arg)).sort((a, b) => b - a);
-    const slicedSortedArr = sortedNumberArr.slice(0, sortedNumberArr.length - k);
-    return slicedSortedArr.join('');
+    
 };
 
 console.log(solution("1924", 2));
--- programmers-조이스틱/programmers-42860.js
+++
@@ -1,7 +0,0 @@
-const solution = (number, k) => {
-    
-};
-
-console.log(solution("1924", 2));
-console.log(solution("1231234", 3));
-console.log(solution("4177252841", 4));
