ee617a5a58423b73e42a528a7fda34da098ae05d
Inseob Jeon <sonofhuman20@gmail.com>
2021-02-05 22:22:06 +0900

    21.02.05 solved

--- 이것이코딩테스트다/5-Search(DFS, BFS)/5-EX-1-factorial.js
+++ 이것이코딩테스트다/5-Search(DFS, BFS)/5-EX-1-factorial.js
@@ -1,25 +1,18 @@
-// 일정한 수 n 을 입력받아 n에서부터 1까지의 모든 수의 곱인 팩토리얼(factorial) 값을 반환하는 함수
-
-// 재귀함수 버전
-// 유의사항이 있다면, 재귀함수 자체를 return 해주어야 한다는 점이다. 
-const factorialWithRecursion = (n) => {
-  if (n <= 1) {
-    return 1;
+const factorialWithIteration = (targetNum) => {
+  let accm = 0;
+  for (let i = 1; i <= targetNum; i++) {
+    accm = accm + i;
   }
-  return n * factorialWithRecursion(n-1);
-}
+  return accm;
+};
 
-console.log(factorialWithRecursion(5));
-
-
-// 반복문 버전
-// 일정한 수 n 을 입력받아 n에서부터 1까지의 모든 수의 곱인 팩토리얼(factorial) 값을 반환하는 함수 
-const factorialWithLoop = (n) => {
-  let returnValue = 1;
-  for (let i = 1; i <= n; i++) {
-    returnValue = returnValue * i;
+// 점화식을 생각하자 = Xn = Xn + @...
+const factorialWithRecursion = (targetNum) => {
+  if (targetNum <= 1) {
+    return targetNum;
   }
-  return returnValue;
-}
+  return targetNum + factorialWithRecursion(targetNum - 1);
+};
 
-console.log(factorialWithLoop(5));
+console.log(factorialWithIteration(10));
+console.log(factorialWithRecursion(10));
