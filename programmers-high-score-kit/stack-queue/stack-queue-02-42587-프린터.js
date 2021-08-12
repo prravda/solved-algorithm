4b0f62bd765939760264ff9a19c7078b93010c92
InseobJeon <is.jeon@chabot.co.kr>
2021-08-13 08:48:52 +0900

    Re-Solved: 2021-08-13(1)

--- programmers-high-score-kit/stack-queue/stack-queue-02-42587-프린터.js
+++ programmers-high-score-kit/stack-queue/stack-queue-02-42587-프린터.js
@@ -30,6 +30,11 @@
  * 4. printerPool 의 length 가 0이 될 때까지 해당 과정을 반복한다.
  * 5. finished 에서 input 으로 들어왔던 location 값이, finished 의 element property 중 order 값인 경우를 찾는다.
  * 6. 해당 element 의 index 를 반환한다.
+ *
+ * [ 오답노트 ]
+ * - 조건을 명확하게 주지 않았다.
+ * - if 조건문을 만족하지 않는 모든 경우가 아니라, 또 다른 조건을 만족하는 경우를 명시해주었어야 했는데,
+ *   그러지 않다 보니 내가 생각한 대로 연산을 하지 못 했다.
  */
 
 function solution(priorities, location) {
@@ -42,23 +47,50 @@ function solution(priorities, location) {
   const finishedTask = [];
 
   while (printerPool.length > 0) {
-    if (
-      printerPool.some(
-        (eachTask) => eachTask.priority > printerPool[0].priority
-      )
-    ) {
-      const taskToRemoved = printerPool.shift();
-      printerPool.push(taskToRemoved);
+    // 1개만 남은 경우 -> 그것을 finishedTask 에 넣어준다.
+    if (printerPool.length === 1) {
+      finishedTask.push(printerPool.shift());
     }
-    finishedTask.push(printerPool.shift());
+
+    // 2개 이상의 task 가 남았고,
+    // printerPool[0] 의 우선순위보다 더 중요한 문서가 뒤에 있다면
+    // -> 해당 task 는 맨 뒤로 미뤄준다
+    if (
+      printerPool.length > 1 &&
+      printerPool
+        .slice(1)
+        .some((task) => task.priority > printerPool[0].priority)
+    ) {
+      printerPool.push(printerPool.shift());
+    }
+
+    // 2개 이상의 task 가 남았고,
+    // printerPool[0] 의 우선순위보다 더 중요한 문서가 뒤에 없다면
+    // -> 해당 task 를 .pop() 한 뒤, finishedTask 로 push() 해 준다
+    if (
+      printerPool.length > 1 &&
+      !printerPool
+        .slice(1)
+        .some((task) => task.priority > printerPool[0].priority)
+    ) {
+      finishedTask.push(printerPool.shift());
+    }
   }
 
+  // finishedTask 를 순회한다.
+  // 그리고, finishedTask 를 구성하는 element 의 order property 가 location 과 일치하는 경우
+  // -> i 를 반환하고, 연산을 종료한다.
   for (let i = 0; i < finishedTask.length; i++) {
     if (finishedTask[i].order === location) {
-      return i;
+      return i + 1;
     }
   }
 }
 
-const result1 = solution([1, 1, 9, 1, 1, 1], 0);
-console.log(result1);
+solution([2, 1, 3, 2], 2);
+
+describe('test code', () => {
+  it('first case', () => {
+    expect(solution([2, 1, 3, 2], 2)).toBe(1);
+  });
+});
